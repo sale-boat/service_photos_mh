de6306977d077a1c701b7188963bc6ea9fc3ae34
Michael Hamm <mikejameshamm@gmail.com>
2019-03-10 13:29:55 -0700

    Create postgres schema files

---
+++ app/models/postgres/schema.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  unique_id int,
+  name varchar(100),
+  slug varchar(125),
+  category varchar,
+  manufacturer varchar,
+  primary_image varchar(100),
+  secondary_images varchar(100)[],
+  review_one_star_count int,
+  review_two_star_count int,
+  review_three_star_count int,
+  review_four_star_count int,
+  review_five_star_count int,
+  review_count int,
+  question_count int,
+  price int,
+  total_price int,
+  stock int,
+  is_prime boolean,
+  description varchar(250)
+);
---
+++ app/models/postgres/seed.sql
@@ -0,0 +1,22 @@
+
+COPY products(
+  unique_id, 
+  name,
+  slug,
+  category,
+  manufacturer, 
+  primary_image, 
+  secondary_images,
+  review_one_star_count, 
+  review_two_star_count, 
+  review_three_star_count, 
+  review_four_star_count,
+  review_five_star_count,
+  review_count,
+  question_count,
+  price,
+  total_price,
+  stock,
+  is_prime,
+  description
+) FROM '/home/ubuntu/service_photos_mh/dataGeneration/output.csv' DELIMITER ',' CSV HEADER;
